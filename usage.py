69502a5a87e8d3d40148d328c3dde6f66abb7325
Anders Fredrik Kiær <akia@statoil.com>
2019-08-19 13:19:46 +0200

    Update usage.py

--- usage.py
+++ usage.py
@@ -1,12 +1,6 @@
-import webviz_core_components
 import dash
-import numpy as np
-from dash.dependencies import Input, Output
 import dash_html_components as html
-import pandas as pd
-
-df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-                  columns=['a', 'b', 'c'])
+import webviz_core_components
 
 app = dash.Dash(__name__)
 
@@ -17,14 +11,6 @@ app.layout = html.Div([
     ),
     html.Div(id='output')
 ])
-
-@app.callback(Output('container', 'csv_string'),
-              [Input('container', 'csv_requested')])
-def return_csv_string(csv_requested):
-    if not csv_requested:
-        return ''
-    else:
-        return df.to_csv()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
