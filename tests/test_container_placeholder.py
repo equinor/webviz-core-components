13665065c9d2ebbb5499fb209b90a0977a5280bd
Anders Fredrik Kiær <akia@statoil.com>
2019-08-19 14:34:54 +0200

    Change build script Travis

--- tests/test_container_placeholder.py
+++ tests/test_container_placeholder.py
@@ -1,25 +1,17 @@
-from dash.testing.application_runners import import_app
+import dash
+import dash_html_components as html
+import webviz_core_components
 
+def test_container_placeholder(dash_duo):
 
-# Basic test for the component rendering.
-# The dash_duo pytest fixture is installed with dash (v1.0+)
-def test_render_component(dash_duo):
-    # Start a dash app contained as the variable `app` in `usage.py`
-    app = import_app('usage')
+    app = dash.Dash(__name__)
+
+    app.layout = html.Div([
+        webviz_core_components.WebvizContainerPlaceholder(
+            id='container',
+            children=['Hello world']
+        ),
+        html.Div(id='output')
+    ])
+
     dash_duo.start_server(app)
-
-    # Get the generated component input with selenium
-    # The html input will be a children of the #input dash component
-    my_component = dash_duo.find_element('#input > input')
-
-    assert 'my-value' == my_component.get_attribute('value')
-
-    # Clear the input
-    dash_duo.clear_input(my_component)
-
-    # Send keys to the custom input.
-    my_component.send_keys('Hello dash')
-
-    # Wait for the text to equal, if after the timeout (default 10 seconds)
-    # the text is not equal it will fail the test.
-    dash_duo.wait_for_text_to_equal('#output', 'You have entered Hello dash')
