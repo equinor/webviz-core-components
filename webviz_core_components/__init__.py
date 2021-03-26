0d4f7423a5a6af3944a9a43b042eeb5b7aea6aa0
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2021-03-26 16:20:30 +0100

    Adjusted too long lines

--- webviz_core_components/__init__.py
+++ webviz_core_components/__init__.py
@@ -42,7 +42,13 @@ _filepath = _os.path.abspath(_os.path.join(_basepath, "package-info.json"))
 with open(_filepath) as f:
     package = json.load(f)
 
-package_name = package["name"].replace(" ", "_").replace("-", "_").replace("/", "_").replace("@", "")
+package_name = (
+    package["name"]
+    .replace(" ", "_")
+    .replace("-", "_")
+    .replace("/", "_")
+    .replace("@", "")
+)
 
 _current_path = _os.path.dirname(_os.path.abspath(__file__))
 
