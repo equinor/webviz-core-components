9930ef6e8f63e4dfc0f81ee95e1424cec51bfb4a
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2021-06-02 11:55:51 +0200

    Fixed formatting issues.

--- react/src/lib/components/SmartNodeSelector/utils/TreeData.ts
+++ react/src/lib/components/SmartNodeSelector/utils/TreeData.ts
@@ -5,8 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import { TreeDataNode, TreeDataNodeMetaData } from './TreeDataNodeTypes';
-
+import { TreeDataNode, TreeDataNodeMetaData } from "./TreeDataNodeTypes";
 
 export default class TreeData {
     private treeData: TreeDataNode[];
@@ -16,7 +15,7 @@ export default class TreeData {
 
     constructor({
         treeData,
-        delimiter
+        delimiter,
     }: {
         treeData: TreeDataNode[];
         delimiter: string;
@@ -34,75 +33,89 @@ export default class TreeData {
         let stringifiedData = "";
         const nodeData: TreeDataNodeMetaData[] = [];
         const delimiter = this.delimiter;
-        const populateNode = (indices: number[] = [], nodePath: string): void => {
+        const populateNode = (
+            indices: number[] = [],
+            nodePath: string
+        ): void => {
             if (indices.length == 0) {
                 throw "Indices array must at least have one element";
             }
             let node: TreeDataNode = this.treeData[indices[0]];
             for (let i = 1; i < indices.length; i++) {
-                if (node.children)
-                    node = node.children[indices[i]];
-                else
-                    throw "Implementation error";
+                if (node.children) node = node.children[indices[i]];
+                else throw "Implementation error";
             }
-            if (node.name === "" || node.name === undefined || node.name === null) {
-                const path = nodePath.replace(/\{[0-9]+\}/g, "") + (nodePath !== "" ? delimiter : "") + node.name;
+            if (
+                node.name === "" ||
+                node.name === undefined ||
+                node.name === null
+            ) {
+                const path =
+                    nodePath.replace(/\{[0-9]+\}/g, "") +
+                    (nodePath !== "" ? delimiter : "") +
+                    node.name;
                 throw `
                     Empty/invalid strings are not allowed as names of nodes:
                     "${path}"
                     ${Array(path.length + 2).join("\u00A0")}^`;
-                
             }
             nodeData.push({
                 id: node.id,
                 description: node.description,
                 color: node.color,
                 icon: node.icon,
-                numChildren: (node.children ? node.children.length : 0)
+                numChildren: node.children ? node.children.length : 0,
             });
             const index = indexCount++;
             if (node.children && node.children.length > 0) {
                 for (let i = 0; i < node.children.length; i++) {
                     populateNode(
                         [...indices, i],
-                        `${nodePath}${nodePath != "" ? delimiter : ""}{${index}}${node.name}`
+                        `${nodePath}${
+                            nodePath != "" ? delimiter : ""
+                        }{${index}}${node.name}`
                     );
                 }
+            } else {
+                stringifiedData += `"${nodePath}${
+                    nodePath !== "" ? delimiter : ""
+                }{${index}}${node.name}" `;
             }
-            else {
-                stringifiedData += `"${nodePath}${nodePath !== "" ? delimiter : ""}{${index}}${node.name}" `
-            }
-
-        }
+        };
         for (let i = 0; i < this.treeData.length; i++) {
             populateNode([i], "");
         }
         this.stringifiedData = stringifiedData;
         this.nodeData = nodeData;
         if (nodeData.length != indexCount) {
-            throw "implementation error"
+            throw "implementation error";
         }
     }
 
     countMatchedNodes(nodePath: string[]): number {
         let nodePathString = "";
         for (let i = 0; i < nodePath.length - 1; i++) {
-            nodePathString += `\\{(\\d+)\\}${this.adjustNodeName(nodePath[i])}${this.delimiter}`;
+            nodePathString += `\\{(\\d+)\\}${this.adjustNodeName(nodePath[i])}${
+                this.delimiter
+            }`;
         }
 
-        const re = RegExp(`"${nodePathString}\\{(\\d+)\\}([^${this.delimiter}"]*)"`, 'g');
+        const re = RegExp(
+            `"${nodePathString}\\{(\\d+)\\}([^${this.delimiter}"]*)"`,
+            "g"
+        );
         let count = 0;
 
         // Can be replaced with matchAll as soon as ECMAScript 2021 is declared standard in this project.
         // see: https://tc39.es/ecma262/#sec-string.prototype.matchall
-        while ((re.exec(this.stringifiedData)) !== null) {
+        while (re.exec(this.stringifiedData) !== null) {
             count++;
         }
         return count;
     }
 
     private escapeRegExp(string: string): string {
-        return string.replace(/[.+^${}()|[]\\]/g, '\\$&');
+        return string.replace(/[.+^${}()|[]\\]/g, "\\$&");
     }
 
     private replaceAll(str: string, find: string, replace: string): string {
@@ -110,10 +123,17 @@ export default class TreeData {
     }
 
     private adjustNodeName(nodeName: string): string {
-        return this.replaceAll(this.replaceAll(this.escapeRegExp(nodeName), "*", "[^:\"]*"), "?", ".");
+        return this.replaceAll(
+            this.replaceAll(this.escapeRegExp(nodeName), "*", '[^:"]*'),
+            "?",
+            "."
+        );
     }
 
-    findFirstNode(nodePath: string[], completeNodePath = true): TreeDataNodeMetaData[] | null {
+    findFirstNode(
+        nodePath: string[],
+        completeNodePath = true
+    ): TreeDataNodeMetaData[] | null {
         let nodePathString = "";
         for (let i = 0; i < nodePath.length; i++) {
             if (i > 0) {
@@ -133,16 +153,26 @@ export default class TreeData {
         return result;
     }
 
-    findSuggestions(nodePath: string[]): { nodeName: string; metaData: TreeDataNodeMetaData }[] {
+    findSuggestions(
+        nodePath: string[]
+    ): { nodeName: string; metaData: TreeDataNodeMetaData }[] {
         const searchTerm = this.adjustNodeName(nodePath[nodePath.length - 1]);
         let nodePathString = "";
         for (let i = 0; i < nodePath.length - 1; i++) {
-            nodePathString += `\\{(\\d+)\\}${this.adjustNodeName(nodePath[i])}${this.delimiter}`;
+            nodePathString += `\\{(\\d+)\\}${this.adjustNodeName(nodePath[i])}${
+                this.delimiter
+            }`;
         }
 
-        const re = RegExp(`"${nodePathString}\\{(\\d+)\\}(${searchTerm}[^${this.delimiter}"]*)`, 'g');
+        const re = RegExp(
+            `"${nodePathString}\\{(\\d+)\\}(${searchTerm}[^${this.delimiter}"]*)`,
+            "g"
+        );
 
-        const suggestions: { nodeName: string; metaData: TreeDataNodeMetaData }[] = [];
+        const suggestions: {
+            nodeName: string;
+            metaData: TreeDataNodeMetaData;
+        }[] = [];
         const nodeNames: Set<string> = new Set();
 
         // Can be replaced with matchAll as soon as ECMAScript 2021 is declared standard in this project.
@@ -157,23 +187,31 @@ export default class TreeData {
 
             suggestions.push({
                 nodeName: match[nodePath.length + 1],
-                metaData: this.nodeData[parseInt(match[nodePath.length + 0])]
+                metaData: this.nodeData[parseInt(match[nodePath.length + 0])],
             });
         }
         return suggestions;
     }
 
-    findChildNodes(nodePath: string[]): { nodeName: string; metaData: TreeDataNodeMetaData }[] {
+    findChildNodes(
+        nodePath: string[]
+    ): { nodeName: string; metaData: TreeDataNodeMetaData }[] {
         let nodePathString = "";
         for (let i = 0; i < nodePath.length; i++) {
-            nodePathString += `\\{(\\d+)\\}${this.adjustNodeName(nodePath[i])}${this.delimiter}`;
+            nodePathString += `\\{(\\d+)\\}${this.adjustNodeName(nodePath[i])}${
+                this.delimiter
+            }`;
         }
 
         const re = RegExp(
-            `"${nodePathString}\\{(\\d+)\\}([^${this.delimiter}"]*)`, 'g'
+            `"${nodePathString}\\{(\\d+)\\}([^${this.delimiter}"]*)`,
+            "g"
         );
 
-        const childNodes: { nodeName: string; metaData: TreeDataNodeMetaData }[] = [];
+        const childNodes: {
+            nodeName: string;
+            metaData: TreeDataNodeMetaData;
+        }[] = [];
         const nodeNames: Set<string> = new Set();
 
         // Can be replaced with matchAll as soon as ECMAScript 2021 is declared standard in this project.
@@ -188,17 +226,20 @@ export default class TreeData {
 
             childNodes.push({
                 nodeName: match[nodePath.length + 2],
-                metaData: this.nodeData[parseInt(match[nodePath.length + 1])]
+                metaData: this.nodeData[parseInt(match[nodePath.length + 1])],
             });
         }
         return childNodes;
     }
 
     cleanNodeName(nodeName: string): string {
-        return nodeName.replace(new RegExp(`\\{\\d+\\}`, 'g'), "");
+        return nodeName.replace(new RegExp(`\\{\\d+\\}`, "g"), "");
     }
 
-    findNodes(nodePath: string[], exactMatch: boolean = false): { nodePaths: string[]; metaData: TreeDataNodeMetaData[][] } {
+    findNodes(
+        nodePath: string[],
+        exactMatch = false
+    ): { nodePaths: string[]; metaData: TreeDataNodeMetaData[][] } {
         let nodePathString = "";
         for (let i = 0; i < nodePath.length; i++) {
             if (i > 0) {
@@ -206,7 +247,9 @@ export default class TreeData {
             }
             nodePathString += `\\{(\\d+)\\}${this.adjustNodeName(nodePath[i])}`;
         }
-        const re = exactMatch ? RegExp(`"(${nodePathString})"`, 'g') : RegExp(`"(${nodePathString})`, 'g');
+        const re = exactMatch
+            ? RegExp(`"(${nodePathString})"`, "g")
+            : RegExp(`"(${nodePathString})`, "g");
 
         const metaData: TreeDataNodeMetaData[][] = [];
         const nodePaths: string[] = [];
@@ -224,7 +267,7 @@ export default class TreeData {
         }
         return {
             nodePaths: nodePaths,
-            metaData: metaData
+            metaData: metaData,
         };
     }
 }
