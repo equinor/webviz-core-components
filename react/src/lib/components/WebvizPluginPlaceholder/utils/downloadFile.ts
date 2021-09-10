600b25a07763926ac5f7758012a7a3ccdb48cf29
Ruben Thoms <ruben.thoms@ceetronsolutions.com>
2021-09-10 10:32:24 +0200

    Call `revokeObjectURL` after using `createObjectURL`

--- react/src/lib/components/WebvizPluginPlaceholder/utils/downloadFile.ts
+++ react/src/lib/components/WebvizPluginPlaceholder/utils/downloadFile.ts
@@ -5,21 +5,20 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-export default function downloadFile(
-    {
-        filename,
-        data,
-        mimeType
-    }: {
-        filename: string,
-        data: Blob | string,
-        mimeType: string
-    }
-): void {
+export default function downloadFile({
+    filename,
+    data,
+    mimeType,
+}: {
+    filename: string;
+    data: Blob | string;
+    mimeType: string;
+}): void {
+    let src: string | undefined = undefined;
     const link = document.createElement("a");
-
     if (data instanceof Blob) {
-        link.setAttribute("href", URL.createObjectURL(data));
+        src = URL.createObjectURL(data);
+        link.setAttribute("href", src);
     } else {
         link.setAttribute("href", `data:${mimeType};base64,${data}`);
     }
@@ -30,4 +29,7 @@ export default function downloadFile(
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
+    if (src) {
+        URL.revokeObjectURL(src);
+    }
 }
