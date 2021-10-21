f07ede6740126f6367ca5b4820593e88764fa598
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2021-10-21 09:55:29 +0200

    Add collapsible option to

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - [#177](https://github.com/equinor/webviz-core-components/pull/177) - Bug fix: Menu missing if using non-existent icon.
 
+### Added
+
+- [#182](https://github.com/equinor/webviz-core-components/pull/182) - Added option to wrap `SelectWithLabel` in a `Details` collapsible widget.
+
 ## [0.5.2] - 2021-10-08
 
 ### Changed
--- webviz_core_components/wrapped_components/select_with_label.py
+++ webviz_core_components/wrapped_components/select_with_label.py
@@ -12,6 +12,12 @@ class SelectWithLabel(html.Div):
     - label (string; optional):
         The text of the label
 
+    - collapsible (bool; optional):
+        Wraps the select in a collapsible box
+
+    - open_details (bool; default True):
+        Used to set initial opened/closed state if the collapsible box is used
+
     - wrapper_id (string; optional):
         Id of the wrapping div
 
@@ -33,6 +39,8 @@ class SelectWithLabel(html.Div):
     def __init__(
         self,
         label: str = None,
+        collapsible: bool = False,
+        open_details: bool = True,
         wrapper_id: str = None,
         persistence: bool = True,
         persistence_type: str = "session",
@@ -41,7 +49,10 @@ class SelectWithLabel(html.Div):
         super().__init__()
         if wrapper_id is not None:
             self.id = wrapper_id
-        children = [html.Label(label)] if label else []
+        if collapsible:
+            children = [html.Summary(label)] if label else []
+        else:
+            children = [html.Label(label)] if label else []
         children.append(
             BaseSelect(
                 persistence=persistence,
@@ -49,4 +60,10 @@ class SelectWithLabel(html.Div):
                 **kwargs,
             )
         )
-        self.children = html.Div(style={"fontSize": "15px"}, children=children)
+        if collapsible:
+            self.children = html.Div(
+                style={"fontSize": "15px"},
+                children=html.Details(open=open_details, children=children),
+            )
+        else:
+            self.children = html.Div(style={"fontSize": "15px"}, children=children)
