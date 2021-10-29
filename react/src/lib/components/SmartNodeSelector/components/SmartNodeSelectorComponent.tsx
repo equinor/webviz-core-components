8e38ee4763def7a8f788244799cb0f56e0c59e92
Ruben <ruben.thoms@ceetronsolutions.com>
2021-10-29 15:30:04 +0200

    Removed debug output

--- react/src/lib/components/SmartNodeSelector/components/SmartNodeSelectorComponent.tsx
+++ react/src/lib/components/SmartNodeSelector/components/SmartNodeSelectorComponent.tsx
@@ -1598,7 +1598,6 @@ export default class SmartNodeSelectorComponent extends Component<SmartNodeSelec
 
         return (
             <div id={id} ref={this.ref}>
-                {this.debugOutput()}
                 {label && <label>{label}</label>}
                 <div
                     className={classNames({
