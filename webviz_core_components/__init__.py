e38a0f91b8d7a52f019fbe0bc3ace89d9ab2785c
Ruben <ruben.thoms@ceetronsolutions.com>
2022-06-15 15:56:54 +0200

    Added Python wrapper around `WebvizPluginPlaceholder` for deprecation

--- webviz_core_components/__init__.py
+++ webviz_core_components/__init__.py
@@ -14,6 +14,8 @@ from ._imports_ import __all__
 from .wrapped_components import *
 from .wrapped_components import __all__ as wrapped_components
 
+from .WebvizPluginPlaceholderWrapper import WebvizPluginPlaceholderWrapper as WebvizPluginPlaceholder
+
 __all__ += wrapped_components
 
 try:
