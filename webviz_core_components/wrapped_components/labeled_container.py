e754a1bef13d72cf57d7df869a3010fa591b5d8c
jorgenherje <jorgen.herje@ceetronsolutions.com>
2022-08-17 11:26:24 +0200

    Created labeled container
    - Based on wcc.Selector - without open/close html.Details
    - For margin, label and padding of content in container

---
+++ webviz_core_components/wrapped_components/labeled_container.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+from dash import html
+
+class LabeledContainer(html.Div):
+    """Creates a container with a header label and padded content
+
+    Keyword arguments:
+
+    - children (a list of or a singular dash component, string or number; required):
+        The children of this component.
+
+    - label (str; optional):
+        The text of the label
+
+    - className (string; optional):
+        Additional css class to apply
+
+    """
+
+    def __init__(
+        self,
+        children: Any,
+        label: str = "",
+        className: str = "",
+        **kwargs: Any
+    ) -> None:
+        super().__init__()
+        self.className = className + "webviz-labeled-container"
+        if "id" in kwargs:
+            self.id = kwargs["id"]
+        self.children = [
+            html.Summary(children=label, className="webviz-underlined-label"),
+            html.Div(style={"padding": "10px"}, children=children),
+        ]
