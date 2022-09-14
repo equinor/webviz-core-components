5e36fc1db80ed7aaf56482fc048b61765a6ee955
Ruben <ruben.thoms@ceetronsolutions.com>
2022-09-14 15:18:30 +0200

    `Page` uses store now to access homepage property

--- react/src/lib/components/Menu/components/Page/Page.tsx
+++ react/src/lib/components/Menu/components/Page/Page.tsx
@@ -5,6 +5,7 @@ import { Icon } from "../Icon";
 import { checkIfUrlIsCurrent } from "../../utils/check-url";
 
 import "./Page.css";
+import { useStore } from "../../Menu";
 
 type PageProps = {
     title: string;
@@ -12,15 +13,15 @@ type PageProps = {
     level: number;
     icon?: string;
     applyIconIndentation: boolean;
-    homepage: string;
     firstPage: boolean;
     onClick: () => void;
 };
 
 export const Page: React.FC<PageProps> = (props) => {
+    const store = useStore();
     const active =
         checkIfUrlIsCurrent(props.href) ||
-        (props.firstPage && checkIfUrlIsCurrent(props.homepage));
+        (props.firstPage && checkIfUrlIsCurrent(store.homepage));
 
     return (
         <a
@@ -59,7 +60,7 @@ export const Page: React.FC<PageProps> = (props) => {
 Page.propTypes = {
     title: PropTypes.string.isRequired,
     href: PropTypes.string.isRequired,
-    homepage: PropTypes.string.isRequired,
+    firstPage: PropTypes.bool.isRequired,
     level: PropTypes.number.isRequired,
     icon: PropTypes.string,
     applyIconIndentation: PropTypes.bool.isRequired,
